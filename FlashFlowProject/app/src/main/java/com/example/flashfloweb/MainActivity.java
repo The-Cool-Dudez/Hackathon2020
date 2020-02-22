243a3a313da497fdfe4483a65d470098814643b5
Zachary Hardin <zacharyjhardin@yahoo.com>
2020-02-22 14:34:34 -0600

    Method change I think?

--- FlashFlowProject/app/src/main/java/com/example/flashfloweb/MainActivity.java
+++ FlashFlowProject/app/src/main/java/com/example/flashfloweb/MainActivity.java
@@ -46,7 +46,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void onClick(View view)
     {
-        
+        switch (view.getId())
+        {
+
+        }
     }
 
 }
