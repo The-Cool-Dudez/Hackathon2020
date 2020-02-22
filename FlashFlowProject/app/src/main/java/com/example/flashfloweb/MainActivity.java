6fcf350d629ddf4f5561144408d662089e61a7d5
endoffile78 <endoffile78@yahoo.com>
2020-02-22 16:04:38 -0600

    Get rid of unused imports

--- FlashFlowProject/app/src/main/java/com/example/flashfloweb/MainActivity.java
+++ FlashFlowProject/app/src/main/java/com/example/flashfloweb/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.flashfloweb;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,7 +11,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
