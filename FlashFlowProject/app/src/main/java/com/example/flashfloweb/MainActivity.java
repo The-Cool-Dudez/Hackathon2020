03db7ec7d2dc0a39b33b4c8a7339fe2dbdbd7d70
Zachary Hardin <zacharyjhardin@yahoo.com>
2020-02-22 15:04:11 -0600

    Update MainActivity.java

--- FlashFlowProject/app/src/main/java/com/example/flashfloweb/MainActivity.java
+++ FlashFlowProject/app/src/main/java/com/example/flashfloweb/MainActivity.java
@@ -11,45 +11,61 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button button1 = findViewById(R.id.btnConnect);
+        Button button2 = findViewById(R.id.btnUpload);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void onClick(View view)
+    @Override
+    public void onClick(View v)
     {
-        switch (view.getId())
+        switch (v.getId())
         {
-
+            case btnConnect:
+                btnConnect.Text = "LOL";
+                break;
+            case btnUpload:
+                btnuUpload.Text = "LOL";
+                break;
         }
     }
-
 }
